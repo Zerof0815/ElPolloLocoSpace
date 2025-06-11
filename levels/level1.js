7db432926afef7ab9b1d2808277011dc1f46d390
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-06-11 21:11:04 +0200

    created level files for better code

---
+++ levels/level1.js
@@ -0,0 +1,42 @@
+const level1 = new Level(
+    [
+      new Background(
+      BACKGROUND,
+      0,
+      0,
+      720,
+      480
+    ),
+    new Asteroid(
+      ASTEROIDS.PLANET,
+      Math.floor(Math.random() * 721),
+      Math.floor(Math.random() * 380),
+      100,
+      100,
+      0.3
+    ),
+    new Asteroid(
+      ASTEROIDS.ROCK,
+      Math.floor(Math.random() * 721),
+      Math.floor(Math.random() * 380),
+      50,
+      50,
+      1.5
+    ),
+    new Asteroid(
+      ASTEROIDS.ROCK,
+      Math.floor(Math.random() * 721),
+      Math.floor(Math.random() * 380),
+      25,
+      25,
+      1.5
+    )
+    ],
+    [
+      new Chicken(CHICKEN_IMAGES.NORMAL[0], 75, 75, 3, CHICKEN_IMAGES.NORMAL),
+      new Chicken(CHICKEN_IMAGES.NORMAL[0], 75, 75, 3, CHICKEN_IMAGES.NORMAL),
+      new Chicken(CHICKEN_IMAGES.NORMAL[0], 75, 75, 3, CHICKEN_IMAGES.NORMAL),
+      new Chicken(CHICKEN_IMAGES.SMALL[0], 50, 50, 4, CHICKEN_IMAGES.SMALL),
+      new Chicken(CHICKEN_IMAGES.SMALL[0], 50, 50, 4, CHICKEN_IMAGES.SMALL),
+    ]
+)
--- models/world.class.js
+++ models/world.class.js
@@ -1,45 +1,7 @@
 class World {
-  background = [
-    new Background(
-      BACKGROUND,
-      0,
-      0,
-      720,
-      480
-    ),
-    new Asteroid(
-      ASTEROIDS.PLANET,
-      Math.floor(Math.random() * 721),
-      Math.floor(Math.random() * 380),
-      100,
-      100,
-      0.3
-    ),
-    new Asteroid(
-      ASTEROIDS.ROCK,
-      Math.floor(Math.random() * 721),
-      Math.floor(Math.random() * 380),
-      50,
-      50,
-      1.5
-    ),
-    new Asteroid(
-      ASTEROIDS.ROCK,
-      Math.floor(Math.random() * 721),
-      Math.floor(Math.random() * 380),
-      25,
-      25,
-      1.5
-    ),
-  ];
   character = new Character();
-  enemies = [
-    new Chicken(CHICKEN_IMAGES.NORMAL[0], 75, 75, 3, CHICKEN_IMAGES.NORMAL),
-    new Chicken(CHICKEN_IMAGES.NORMAL[0], 75, 75, 3, CHICKEN_IMAGES.NORMAL),
-    new Chicken(CHICKEN_IMAGES.NORMAL[0], 75, 75, 3, CHICKEN_IMAGES.NORMAL),
-    new Chicken(CHICKEN_IMAGES.SMALL[0], 50, 50, 4, CHICKEN_IMAGES.SMALL),
-    new Chicken(CHICKEN_IMAGES.SMALL[0], 50, 50, 4, CHICKEN_IMAGES.SMALL),
-  ];
+  background = level1.background;
+  enemies = level1.enemies;
   canvas;
   ctx;
   keyboard;
