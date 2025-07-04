e8acb7a255f9f5690157ea4a10076a03d6268494
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-07-04 17:24:39 +0200

    repaired paths

--- scripts/imagePaths.js
+++ scripts/imagePaths.js
@@ -1,46 +1,46 @@
 const CHICKEN_IMAGES = {
   NORMAL: [
-    "../assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
-    "../assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
-    "../assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+    "assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
   ],
   SMALL: [
-    "../assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
-    "../assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
-    "../assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+    "assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
   ],
   NORMAL_DEAD: "assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png",
   SMALL_DEAD: "assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png"
 };
 
 const ENDBOSS = {
-    WALK: [
-      "../assets/img/4_enemie_boss_chicken/1_walk/G1.png",
-      "../assets/img/4_enemie_boss_chicken/1_walk/G2.png",
-      "../assets/img/4_enemie_boss_chicken/1_walk/G3.png",
-      "../assets/img/4_enemie_boss_chicken/1_walk/G4.png"
-    ],
-    ALERT: [
-      "../assets/img/4_enemie_boss_chicken/2_alert/G5.png",
-      "../assets/img/4_enemie_boss_chicken/2_alert/G6.png",
-      "../assets/img/4_enemie_boss_chicken/2_alert/G7.png",
-      "../assets/img/4_enemie_boss_chicken/2_alert/G8.png",
-      "../assets/img/4_enemie_boss_chicken/2_alert/G9.png",
-      "../assets/img/4_enemie_boss_chicken/2_alert/G10.png",
-      "../assets/img/4_enemie_boss_chicken/2_alert/G11.png",
-      "../assets/img/4_enemie_boss_chicken/2_alert/G12.png"
-    ],
-    ATTACK: [
-      "../assets/img/4_enemie_boss_chicken/3_attack/G13.png",
-      "../assets/img/4_enemie_boss_chicken/3_attack/G14.png",
-      "../assets/img/4_enemie_boss_chicken/3_attack/G15.png",
-      "../assets/img/4_enemie_boss_chicken/3_attack/G16.png",
-      "../assets/img/4_enemie_boss_chicken/3_attack/G17.png",
-      "../assets/img/4_enemie_boss_chicken/3_attack/G18.png",
-      "../assets/img/4_enemie_boss_chicken/3_attack/G19.png",
-      "../assets/img/4_enemie_boss_chicken/3_attack/G20.png"
-    ]
-}
+  WALK: [
+    "assets/img/4_enemie_boss_chicken/1_walk/G1.png",
+    "assets/img/4_enemie_boss_chicken/1_walk/G2.png",
+    "assets/img/4_enemie_boss_chicken/1_walk/G3.png",
+    "assets/img/4_enemie_boss_chicken/1_walk/G4.png"
+  ],
+  ALERT: [
+    "assets/img/4_enemie_boss_chicken/2_alert/G5.png",
+    "assets/img/4_enemie_boss_chicken/2_alert/G6.png",
+    "assets/img/4_enemie_boss_chicken/2_alert/G7.png",
+    "assets/img/4_enemie_boss_chicken/2_alert/G8.png",
+    "assets/img/4_enemie_boss_chicken/2_alert/G9.png",
+    "assets/img/4_enemie_boss_chicken/2_alert/G10.png",
+    "assets/img/4_enemie_boss_chicken/2_alert/G11.png",
+    "assets/img/4_enemie_boss_chicken/2_alert/G12.png",
+  ],
+  ATTACK: [
+    "assets/img/4_enemie_boss_chicken/3_attack/G13.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G14.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G15.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G16.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G17.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G18.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G19.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G20.png",
+  ],
+};
 
 const EXPLOSION = [
   "assets/img/10_explosion_effect/explosion-f1.png",
@@ -54,18 +54,18 @@ const EXPLOSION = [
 ]
 
 const BACKGROUND =
-  "../assets/img/5_background/Blue Version/layered/blue-back.png";
+  "assets/img/5_background/Blue Version/layered/blue-back.png";
 
 const ASTEROIDS = {
-  PLANET: "../assets/img/5_background/Blue Version/layered/prop-planet-big.png",
-  ROCK: "../assets/img/5_background/Blue Version/layered/asteroid-2.png"
+  PLANET: "assets/img/5_background/Blue Version/layered/prop-planet-big.png",
+  ROCK: "assets/img/5_background/Blue Version/layered/asteroid-2.png"
 }
 
 const PEPE_ANIMATION = {
   HURT: [
-    "../assets/img/2_character_pepe/4_hurt/H-41.png",
-    "../assets/img/2_character_pepe/4_hurt/H-42.png",
-    "../assets/img/2_character_pepe/4_hurt/H-43.png"
+    "assets/img/2_character_pepe/4_hurt/H-41.png",
+    "assets/img/2_character_pepe/4_hurt/H-42.png",
+    "assets/img/2_character_pepe/4_hurt/H-43.png"
   ],
 };
 
